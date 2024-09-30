e67df439a8429b63b208ca3e9839d648e39b62fd
Reynard A. <reynardaurele1510@gmail.com>
2024-09-30 12:22:32 +0700

    -

--- src/Main.kt
+++ src/Main.kt
@@ -20,39 +20,33 @@ fun main() {
 
     }
 
+    print("Enter Book ID: ")
+    val id = readLine()?.toIntOrNull() ?: run {
+        println("Invalid ID. Please enter a valid integer.")
+        return
+    }
 
+    // Input Book Title
+    print("Enter Title: ")
+    val title = readLine()?.takeIf { it.isNotBlank() } ?: run {
+        println("Title cannot be empty.")
+        return
+    }
 
+    // Input Book Author
+    print("Enter Author: ")
+    val author = readLine()?.takeIf { it.isNotBlank() } ?: run {
+        println("Author cannot be empty.")
+        return
+    }
 
+    // Input Book Genre
+    print("Enter Genre: ")
+    val genre = readLine() ?: run {
+        println("Genre cannot be empty.")
+        return
+    }
 
-//    val addBook = AddBook()
-//
-//    print("Enter Book ID: ")
-//    val id = readLine()?.toIntOrNull() ?: run {
-//        println("Invalid ID. Please enter a valid integer.")
-//        return
-//    }
-//
-//    // Input Book Title
-//    print("Enter Title: ")
-//    val title = readLine()?.takeIf { it.isNotBlank() } ?: run {
-//        println("Title cannot be empty.")
-//        return
-//    }
-//
-//    // Input Book Author
-//    print("Enter Author: ")
-//    val author = readLine()?.takeIf { it.isNotBlank() } ?: run {
-//        println("Author cannot be empty.")
-//        return
-//    }
-//
-//    // Input Book Genre
-//    print("Enter Genre: ")
-//    val genre = readLine() ?: run {
-//        println("Genre cannot be empty.")
-//        return
-//    }
-//
-//    addBook.addBook(Book(id, title, author, genre))
-//    addBook.listBooks()
+    service.addBook(Book(id, title, author, genre))
+    service.listBooks()
 }
